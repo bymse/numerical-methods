93c29d2b6b9462db3d06cb67d0dc7d5fb3aa128e
bymse <egorstrelov@gmail.com>
2021-03-09 23:05:37 +0300

    Output + external epsilons

--- NumericalMethods/ElementaryFunctions.cs
+++ NumericalMethods/ElementaryFunctions.cs
@@ -5,52 +5,70 @@ namespace NumericalMethods
 {
     public static class ElementaryFunctions
     {
-        private const int MAX = 10;
+        public static decimal EpsilonForSin = 0;
+        public static decimal EpsilonForArctg = 0;
+        public static decimal EpsilonForSqrt = 0;
 
         public static decimal Arctg(decimal val)
         {
-            if (Math.Abs(val) < 1)
+            var k = 0;
+            decimal sum = 0;
+            while (true)
             {
-                return Enumerable.Range(0, MAX)
-                    .Sum(k =>
-                        (decimal) Math.Pow(-1, k) *
-                        (decimal) Math.Pow((double) val, 2 * k + 1)
-                        / (2 * k + 1));
-            }
+                if (Math.Abs(val) < 1)
+                {
+                    var item = (decimal) Math.Pow(-1, k) *
+                               (decimal) Math.Pow((double) val, 2 * k + 1)
+                               / (2 * k + 1);
+                    sum += item;
+                    k++;
 
-            return (decimal) Math.PI / 2 * Math.Sign(val)
-                   - Enumerable.Range(0, MAX)
-                       .Sum(k =>
-                           (decimal) Math.Pow(-1, k) *
-                           (decimal) Math.Pow((double) val, -(2 * k + 1))
-                           / (2 * k + 1));
+                    if (Math.Abs(sum) < Math.Abs(EpsilonForArctg))
+                    {
+                        return sum;
+                    }
+                }
+                else
+                {
+                    throw new Exception("Val is greater than one on custom arctg");
+                }
+            }
         }
 
         public static decimal Sin(decimal val)
         {
-            return Enumerable.Range(0, MAX)
-                .Sum(k =>
-                {
-                    var factorial = Factorial(2 * k + 1);
-                    return (decimal) Math.Pow(-1, k) *
+            var k = 0;
+            decimal sum = 0;
+            while (true)
+            {
+                var factorial = (decimal) Factorial(2 * k + 1);
+                var item = (decimal) Math.Pow(-1, k) *
                            (decimal) Math.Pow((double) val, 2 * k + 1)
                            / factorial;
-                });
+                sum += item;
+                k++;
+
+                if (Math.Abs(item) < Math.Abs(EpsilonForSin))
+                {
+                    return sum;
+                }
+            }
         }
 
         public static int Factorial(int val) => Enumerable.Range(1, val).Aggregate(1, (p, item) => p * item);
 
-        public static decimal Sqrt(decimal n) 
+        public static decimal Sqrt(decimal n)
         {
-            var x = n; 
-            decimal y = 1; 
-  
-            const decimal e = 0.000001M; 
-            while (x - y > e) { 
-                x = (x + y) / 2; 
-                y = n / x; 
-            } 
-            return x; 
+            var x = n;
+            decimal y = 1;
+
+            while (x - y > EpsilonForSqrt)
+            {
+                x = (x + y) / 2;
+                y = n / x;
+            }
+
+            return x;
         }
     }
 }
--- NumericalMethods/Program.cs
+++ NumericalMethods/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
+using System.Globalization;
+using System.IO;
 
 namespace NumericalMethods
 {
@@ -8,23 +8,97 @@ namespace NumericalMethods
     {
         static void Main(string[] args)
         {
+            ElementaryFunctions.EpsilonForArctg = ;
+            ElementaryFunctions.EpsilonForSin = ;
+            ElementaryFunctions.EpsilonForSqrt = ;
+            PrintTable();
+        }
+
+        private static void PrintTable()
+        {
             const decimal start = 0.2M;
             const decimal end = 0.3M;
             const decimal shift = 0.01M;
-
-            for (var val = start; val <= end; val+=shift)
+            var writer = new StringWriter(CultureInfo.InvariantCulture);
+            writer.WriteLine("step,g,g',delta(g),fi,fi',delta(fi),psi,psi',delta(psi),z,z',delta(z)");
+            for (var val = start; val <= end; val += shift)
             {
-                var res = Execute(val);
-                Console.WriteLine(res);
+                writer.Write(val);
+                writer.Write(",");
+
+                PrintNativeCustomDiff(val, ExecuteG_Custom, ExecuteG_Native, writer);
+                PrintNativeCustomDiff(val, ExecuteFi_Custom, ExecuteFi_Native, writer);
+                PrintNativeCustomDiff(val, ExecutePsi_Custom, ExecutePsi_Native, writer);
+                PrintNativeCustomDiff(val, Execute_Custom, Execute_Native, writer);
+                writer.WriteLine();
             }
+            File.WriteAllText("out.csv",writer.ToString());
         }
 
-        private static decimal Execute(decimal val)
+        private static void PrintNativeCustomDiff(decimal val,
+            Func<decimal, decimal> custom,
+            Func<decimal, decimal> native, TextWriter writer)
         {
-            var sqrt = ElementaryFunctions.Sqrt(0.9M * val + 1);
-            var arctg = ElementaryFunctions.Arctg(sqrt / (1 - val * val));
-            var sin = ElementaryFunctions.Sin(3 * val + 0.6M);
-            return arctg + sin;
+            var customVal = custom(val);
+            var nativeVal = native(val);
+            var diff = Math.Abs(customVal - nativeVal);
+            var line = $"{customVal},{nativeVal},{diff},";
+            writer.Write(line);
+        }
+
+        private static decimal Execute_Native(decimal val)
+        {
+            return ExecuteFi_Native(val) + ExecutePsi_Native(val);
+        }
+
+        private static decimal Execute_Custom(decimal val)
+        {
+            return ExecuteFi_Custom(val) + ExecutePsi_Custom(val);
+        }
+
+        private static decimal ExecuteG_Custom(decimal val) => ExecuteG(val, ElementaryFunctions.Sqrt);
+        private static decimal ExecuteG_Native(decimal val) => ExecuteG(val, arg => (decimal) Math.Sqrt((double) arg));
+        
+        private static decimal ExecuteFi_Native(decimal val)
+        {
+            return ExecuteFi(val,
+                arg => (decimal) Math.Sqrt((double) arg),
+                arg => (decimal) Math.Atan((double) arg));
+        }
+
+        private static decimal ExecutePsi_Native(decimal val)
+        {
+            return  ExecutePsi(val, arg => (decimal) Math.Sin((double) arg));
+        }
+        
+        private static decimal ExecuteFi_Custom(decimal val)
+        {
+            return ExecuteFi(val,
+                ElementaryFunctions.Sqrt,
+                ElementaryFunctions.Arctg);
+        }
+
+        private static decimal ExecutePsi_Custom(decimal val)
+        {
+            return ExecutePsi(val, ElementaryFunctions.Sin);
+        }
+        
+
+        private static decimal ExecuteFi(decimal val,
+            Func<decimal, decimal> sqrt,
+            Func<decimal, decimal> arctg)
+        {
+            return arctg(ExecuteG(val, sqrt));
+        }
+
+        private static decimal ExecutePsi(decimal val, Func<decimal, decimal> sin)
+        {
+            return sin(3 * val + 0.6M);
+        }
+
+        private static decimal ExecuteG(decimal val, Func<decimal, decimal> sqrt)
+        {
+            return sqrt(0.9M * val + 1) / (1 - val * val);
         }
     }
 }
